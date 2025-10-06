3c15dadd839c965241ac34da31989a5d0b90e44a
Mandy-te <claudysaintjulien123@gmail.com>
2025-10-05 21:46:30 -0400

    Initial commit Flashipping project

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white p-4 mt-10 text-center">
-      <p>© {new Date().getFullYear()} Flashipping. Tout dwa rezève.</p>
-      <p>Kontakte nou: <a href="mailto:info@flashipping.com" className="underline">info@flashipping.com</a></p>
-    </footer>
-  );
-}
