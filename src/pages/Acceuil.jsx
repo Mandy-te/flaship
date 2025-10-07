0bcc20b9ca6af7c12710ba711a8413cfaf0c32b5
Mandy-te <68794982+Mandy-te@users.noreply.github.com>
2025-10-07 07:34:30 -0400

    Create Acceuil.jsx

---
+++ src/pages/Acceuil.jsx
@@ -0,0 +1,8 @@
+export default function Accueil() {
+  return (
+    <section>
+      <h2 className="text-3xl font-semibold mb-4">Bienvenue sur Flashipping 🚀</h2>
+      <p>Découvrez nos services, tarifs et suivez vos colis en temps réel.</p>
+    </section>
+  );
+}
