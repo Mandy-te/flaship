fd877d9c6342ebf604a71597bcbe5b9d1e8764e6
Mandy-te <68794982+Mandy-te@users.noreply.github.com>
2025-10-07 07:33:06 -0400

    Delete src/pages/Footer.jsx

--- src/pages/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white text-center py-4">
-      <p>© {new Date().getFullYear()} Flashipping — Tous droits réservés.</p>
-    </footer>
-  );
-}
