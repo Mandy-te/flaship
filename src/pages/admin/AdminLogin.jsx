9378cf590ee73ba318a6e7120e21da16dc94634f
Mandy-te <68794982+Mandy-te@users.noreply.github.com>
2025-10-15 09:15:56 -0400

    Update AdminLogin.jsx

--- src/pages/admin/AdminLogin.jsx
+++ src/pages/admin/AdminLogin.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../../api";
-import { useAdmin } from "../context/AdminContext";
+import { useAdmin } from "../../context/AdminContext";
 
 export default function AdminLogin() {
   const { loginAdmin } = useAdmin();
