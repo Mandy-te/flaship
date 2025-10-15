aac86f409ed874623b6ae148c08e9408bf29bdb3
Mandy-te <68794982+Mandy-te@users.noreply.github.com>
2025-10-15 12:07:04 -0400

    Update PrivateAdminRoute.jsx

--- src/routes/PrivateAdminRoute.jsx
+++ src/routes/PrivateAdminRoute.jsx
@@ -1,10 +1,10 @@
-// src/routes/PrivateAdminRoute.jsx
 import { Navigate } from "react-router-dom";
 
 const PrivateAdminRoute = ({ children }) => {
   const token = localStorage.getItem("adminToken");
   const role = localStorage.getItem("role");
 
+  // Si pa gen token oswa role ≠ admin → redirection login admin
   if (!token || role !== "admin") {
     return <Navigate to="/admin/login" replace />;
   }
