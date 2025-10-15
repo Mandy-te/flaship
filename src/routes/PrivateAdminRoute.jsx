f15204ed087066992fcf496f4677d555c6d89ce6
Mandy-te <68794982+Mandy-te@users.noreply.github.com>
2025-10-15 09:35:08 -0400

    Update PrivateAdminRoute.jsx

--- src/routes/PrivateAdminRoute.jsx
+++ src/routes/PrivateAdminRoute.jsx
@@ -2,13 +2,12 @@ import { Navigate } from "react-router-dom";
 import { useAdmin } from "../context/AdminContext";
 
 export default function PrivateAdminRoute({ children }) {
-  const { admin, token } = useAdmin();
+  const { admin } = useAdmin();
+  const adminToken = localStorage.getItem("adminToken");
 
-  // Si pa gen admin konekte oswa token pa valide → redireksyon
-  if (!admin || !token) {
+  if (!admin || !adminToken) {
     return <Navigate to="/admin/login" replace />;
   }
 
-  // Sinon, montre paj admin nan
   return children;
 }
